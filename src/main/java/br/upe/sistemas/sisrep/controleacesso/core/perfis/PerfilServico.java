8990b251b098833799e895456cddd34b72f209cf
kvojps <joseferreira.santosjunior@upe.br>
2022-06-11 13:18:22 -0300

    adicionando método de listar os perfis de um usuário específico para gerar dados de tela

--- src/main/java/br/upe/sistemas/sisrep/controleacesso/core/perfis/PerfilServico.java
+++ src/main/java/br/upe/sistemas/sisrep/controleacesso/core/perfis/PerfilServico.java
@@ -1,10 +1,14 @@
 package br.upe.sistemas.sisrep.controleacesso.core.perfis;
 
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 
 import br.upe.sistemas.sisrep.controleacesso.core.usuario.IUsuarioRepositorio;
+import br.upe.sistemas.sisrep.controleacesso.core.usuario.TipoUsuario;
+import br.upe.sistemas.sisrep.controleacesso.core.usuario.Usuario;
 
 public class PerfilServico implements IPerfilServico {
 	
@@ -14,9 +18,19 @@ public class PerfilServico implements IPerfilServico {
 	private IUsuarioRepositorio usuarioRepositorio;
 	
 	@Override
-	public List<Perfil> listar(String email) {
-		// TODO Auto-generated method stub
-		return null;
+	public List<Perfil> listarPerfisDoUsuario(String email) {
+		Usuario usuario = usuarioRepositorio.findByEmail(email);
+		List<Perfil> perfis = new ArrayList<Perfil>();
+		
+		for (Perfil perfil : perfilRepositorio.findAll()) {
+			for (TipoUsuario tipoUsuario : usuario.getPerfis()) {
+				if (perfil.getNome() == tipoUsuario.getNome() ) {
+					perfis.add(perfil);
+				}
+			}
+		}
+		
+		return perfis;
 	}
 
 }
