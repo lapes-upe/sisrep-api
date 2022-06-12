b27604814bd7dac92eefc96f11d6733c1fc76d31
kvojps <joseferreira.santosjunior@upe.br>
2022-06-12 15:02:58 -0300

    adicionando metodo no controlador para obter envolope com dados do usuario

--- src/main/java/br/upe/sistemas/sisrep/controleacesso/api/ControleAcessoAPI.java
+++ src/main/java/br/upe/sistemas/sisrep/controleacesso/api/ControleAcessoAPI.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 import javax.validation.Valid;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,6 +17,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 import br.upe.sistemas.sisrep.controleacesso.api.vos.AssociarPerfilVO;
+import br.upe.sistemas.sisrep.controleacesso.api.vos.DadosUsuarioVO;
 import br.upe.sistemas.sisrep.controleacesso.api.vos.PerfilVO;
 import br.upe.sistemas.sisrep.controleacesso.api.vos.UsuarioVO;
 import br.upe.sistemas.sisrep.controleacesso.core.usuario.IControleAcessoServico;
@@ -32,15 +34,15 @@ public class ControleAcessoAPI {
 
   @GetMapping("/usuarios")
   public ResponseEntity<List<UsuarioVO>> listarUsuarios() {
-    return ResponseEntity
-        .ok(ctrlAcessoServico.listarUsuarios().stream().map(e -> convert(e)).collect(Collectors.toList()));
+    return ResponseEntity.ok(ctrlAcessoServico.listarUsuarios().stream().map(e -> convert(e))
+        .collect(Collectors.toList()));
   }
 
   @PostMapping("/usuario")
   public ResponseEntity<UsuarioVO> incluir(@Valid @RequestBody UsuarioVO usuarioVO) {
 
-    URI uri = URI
-        .create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/controleacesso/usuario").toUriString());
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath()
+        .path("/api/controleacesso/usuario").toUriString());
 
     Usuario usuario = convert(usuarioVO);
     UsuarioVO vo = convert(ctrlAcessoServico.incluir(usuario));
@@ -49,10 +51,11 @@ public class ControleAcessoAPI {
   }
 
   @PutMapping("/usuario/{id}")
-  public ResponseEntity<UsuarioVO> alterar(@PathVariable Long id, @Valid @RequestBody UsuarioVO usuarioVO) {
+  public ResponseEntity<UsuarioVO> alterar(@PathVariable Long id,
+      @Valid @RequestBody UsuarioVO usuarioVO) {
 
-    URI uri = URI
-        .create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/controleacesso/usuario").toUriString());
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath()
+        .path("/api/controleacesso/usuario").toUriString());
 
     Usuario usuario = convert(usuarioVO);
     usuario.setId(id);
@@ -69,8 +72,8 @@ public class ControleAcessoAPI {
 
   @PostMapping("/perfil")
   public ResponseEntity<PerfilVO> incluir(@Valid @RequestBody PerfilVO perfilVO) {
-    URI uri = URI
-        .create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/controleacesso/perfil").toUriString());
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath()
+        .path("/api/controleacesso/perfil").toUriString());
 
     Perfil perfil = convert(perfilVO);
     PerfilVO vo = convert(ctrlAcessoServico.incluir(perfil));
@@ -79,10 +82,11 @@ public class ControleAcessoAPI {
   }
 
   @PutMapping("/perfil/{id}")
-  public ResponseEntity<PerfilVO> alterar(@PathVariable Long id, @Valid @RequestBody PerfilVO perfilVO) {
+  public ResponseEntity<PerfilVO> alterar(@PathVariable Long id,
+      @Valid @RequestBody PerfilVO perfilVO) {
 
-    URI uri = URI
-        .create(ServletUriComponentsBuilder.fromCurrentContextPath().path("/api/controleacesso/perfil").toUriString());
+    URI uri = URI.create(ServletUriComponentsBuilder.fromCurrentContextPath()
+        .path("/api/controleacesso/perfil").toUriString());
 
     Perfil perfil = convert(perfilVO);
     perfil.setId(id);
@@ -99,8 +103,8 @@ public class ControleAcessoAPI {
 
   @GetMapping("/perfis")
   public ResponseEntity<List<PerfilVO>> listarPerfis() {
-    return ResponseEntity
-        .ok(ctrlAcessoServico.listarPerfis().stream().map(e -> convert(e)).collect(Collectors.toList()));
+    return ResponseEntity.ok(ctrlAcessoServico.listarPerfis().stream().map(e -> convert(e))
+        .collect(Collectors.toList()));
   }
 
   @PostMapping("/perfil/associar")
@@ -110,16 +114,27 @@ public class ControleAcessoAPI {
     return ResponseEntity.ok().body(convert(usuario));
   }
 
+  @GetMapping("/obterDadosUsuario")
+  public ResponseEntity<DadosUsuarioVO> obterDadosUsuario(String email) {
+    Usuario usuario = ctrlAcessoServico.buscarUsuarioPorEmail(email);
+
+    DadosUsuarioVO vo = DadosUsuarioVO.builder().email(usuario.getEmail()).nome(usuario.getNome())
+        .endereco(usuario.getEndereco()).cidade(usuario.getCidade()).cep(usuario.getCep())
+        .celular(usuario.getCelular()).build();
+
+    return ResponseEntity.status(HttpStatus.FOUND).body(vo);
+  }
+
   private Usuario convert(UsuarioVO vo) {
 
-    Usuario usuario = Usuario.builder().id(vo.getId()).nome(vo.getNome()).email(vo.getEmail()).cpf(vo.getCpf())
-        .token(vo.getToken()).ativo(vo.getAtivo()).build();
+    Usuario usuario = Usuario.builder().id(vo.getId()).nome(vo.getNome()).email(vo.getEmail())
+        .cpf(vo.getCpf()).token(vo.getToken()).ativo(vo.getAtivo()).build();
 
     if (vo.getPerfis() != null && !vo.getPerfis().isEmpty()) {
       usuario.setPerfis(new ArrayList<>());
 
-      vo.getPerfis().stream()
-          .forEach(p -> usuario.getPerfis().add(Perfil.builder().id(p.getId()).nome(p.getNome()).build()));
+      vo.getPerfis().stream().forEach(
+          p -> usuario.getPerfis().add(Perfil.builder().id(p.getId()).nome(p.getNome()).build()));
     }
 
     return usuario;
@@ -127,14 +142,15 @@ public class ControleAcessoAPI {
 
   private UsuarioVO convert(Usuario entidade) {
 
-    UsuarioVO usuario = UsuarioVO.builder().id(entidade.getId()).nome(entidade.getNome()).email(entidade.getEmail())
-        .cpf(entidade.getCpf()).token(entidade.getToken()).ativo(entidade.getAtivo()).build();
+    UsuarioVO usuario =
+        UsuarioVO.builder().id(entidade.getId()).nome(entidade.getNome()).email(entidade.getEmail())
+            .cpf(entidade.getCpf()).token(entidade.getToken()).ativo(entidade.getAtivo()).build();
 
     if (entidade.getPerfis() != null && !entidade.getPerfis().isEmpty()) {
       usuario.setPerfis(new ArrayList<>());
 
-      entidade.getPerfis().stream()
-          .forEach(p -> usuario.getPerfis().add(PerfilVO.builder().id(p.getId()).nome(p.getNome()).build()));
+      entidade.getPerfis().stream().forEach(
+          p -> usuario.getPerfis().add(PerfilVO.builder().id(p.getId()).nome(p.getNome()).build()));
     }
 
     return usuario;
@@ -145,6 +161,7 @@ public class ControleAcessoAPI {
   }
 
   private PerfilVO convert(Perfil entidade) {
-    return PerfilVO.builder().id(entidade.getId()).nome(entidade.getNome()).icone(entidade.getIcone()).build();
+    return PerfilVO.builder().id(entidade.getId()).nome(entidade.getNome())
+        .icone(entidade.getIcone()).build();
   }
 }
