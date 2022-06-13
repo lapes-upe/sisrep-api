a426211e79a4f8bfd8a905e6dcf15b5328e1f446
Helaine Barreiros <helainelins@gmail.com>
2022-06-13 09:00:08 -0300

    Ajuste de sintaxe no script
    realizando ajuste de sintaxe no script.

--- src/main/resources/db/migration/V5__adicionar_atributos_usuario.sql
+++ src/main/resources/db/migration/V5__adicionar_atributos_usuario.sql
@@ -1,6 +1,6 @@
 ALTER TABLE acesso.usuario
-ADD COLUMN IF NOT EXISTS foto byte[];
-ADD COLUMN IF NOT EXISTS endereco character varying(255);
-ADD COLUMN IF NOT EXISTS cidade character varying(255);
-ADD COLUMN IF NOT EXISTS cep character varying(255);
+ADD COLUMN IF NOT EXISTS foto bytea[],
+ADD COLUMN IF NOT EXISTS endereco character varying(255),
+ADD COLUMN IF NOT EXISTS cidade character varying(255),
+ADD COLUMN IF NOT EXISTS cep character varying(255),
 ADD COLUMN IF NOT EXISTS celular character varying(255);
