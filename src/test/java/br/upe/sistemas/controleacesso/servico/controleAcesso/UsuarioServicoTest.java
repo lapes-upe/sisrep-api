42de95f0537410801b528a843dfbb2cc960d7d99
kvojps <joseferreira.santosjunior@upe.br>
2022-06-15 14:13:13 -0300

    adicionando estrutura da classe de testes referente ao ControleAcessoServico do usuario

--- src/test/java/br/upe/sistemas/controleacesso/servico/controleAcesso/UsuarioServicoTest.java
+++ src/test/java/br/upe/sistemas/controleacesso/servico/controleAcesso/UsuarioServicoTest.java
@@ -1,5 +1,28 @@
 package br.upe.sistemas.controleacesso.servico.controleAcesso;
 
 public class UsuarioServicoTest {
+  public void incluirUsuarioTest() {
+
+  }
+
+  public void alterarUsuarioTest() {
+
+  }
+
+  public void excluirUsuarioTest() {
+
+  }
+
+  public void buscarUsuarioPorEmailTest() {
+
+  }
+
+  public void listarUsuariosTest() {
+
+  }
+
+  public void adicionarPerfilAoUsuarioTeste() {
+
+  }
 
 }
