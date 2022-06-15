4571c81e2a439c152941374cf882397a0bd605d1
kvojps <joseferreira.santosjunior@upe.br>
2022-06-15 14:07:52 -0300

    adicionando estrutura da classe de testes referente ao SistemaServico

--- src/test/java/br/upe/sistemas/controleacesso/servico/sistema/SistemaServicoTest.java
+++ src/test/java/br/upe/sistemas/controleacesso/servico/sistema/SistemaServicoTest.java
@@ -1,5 +1,30 @@
 package br.upe.sistemas.controleacesso.servico.sistema;
 
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import br.upe.sistemas.sisrep.controleacesso.core.servicos.SistemaServico;
+
+@ExtendWith(SpringExtension.class)
 public class SistemaServicoTest {
+
+  @Autowired
+  SistemaServico sistemaServico;
+
+  public void incluirSistemaTeste() {
+
+  }
+
+  public void alterarSistemaTeste() {
+
+  }
+
+  public void excluirSistemaTeste() {
+
+  }
+
+  public void listarSistemasTeste() {
+
+  }
 
 }
