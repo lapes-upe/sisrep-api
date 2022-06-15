d0e376c0948c585e0eb59e6b010d848896cd70f6
kvojps <joseferreira.santosjunior@upe.br>
2022-06-15 14:10:25 -0300

    adicionando estrutura da classe de testes referente ao ControleAcessoServico do perfil

--- src/test/java/br/upe/sistemas/controleacesso/servico/controleAcesso/PerfilServicoTest.java
+++ src/test/java/br/upe/sistemas/controleacesso/servico/controleAcesso/PerfilServicoTest.java
@@ -1,5 +1,19 @@
 package br.upe.sistemas.controleacesso.servico.controleAcesso;
 
 public class PerfilServicoTest {
+  public void incluirPerfilTeste() {
 
+  }
+
+  public void alterarPerfilTeste() {
+
+  }
+
+  public void excluirPerfilTeste() {
+
+  }
+
+  public void listarPerfisTeste() {
+
+  }
 }
