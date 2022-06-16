4f40c1810ce44b471c7751060cd5ac8f5fb6a59d
kvojps <joseferreira.santosjunior@upe.br>
2022-06-16 00:56:36 -0300

    finalizando entidades chaves do sisrep

--- src/main/java/br/upe/sistemas/sisrep/sisrep/modelo/FichaDispensa.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/modelo/FichaDispensa.java
@@ -1,14 +1,10 @@
 package br.upe.sistemas.sisrep.sisrep.modelo;
 
 import java.util.List;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
 import lombok.Data;
 
 @Data
-@Entity
 public class FichaDispensa {
-  @OneToMany
   private List<Disciplina> disciplina;
   private StatusDisciplinaEnum status;
 }
--- src/main/java/br/upe/sistemas/sisrep/sisrep/modelo/InstituicaoEnsino.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/modelo/InstituicaoEnsino.java
@@ -1,10 +1,8 @@
 package br.upe.sistemas.sisrep.sisrep.modelo;
 
-import javax.persistence.Entity;
 import lombok.Data;
 
 @Data
-@Entity
 public class InstituicaoEnsino {
   private String nome;
 }
