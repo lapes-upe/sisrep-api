7ea4276d3f59191b3917b7b0a1be1335cfb64e94
kvojps <joseferreira.santosjunior@upe.br>
2022-06-17 18:58:31 -0300

    adicionando servicos a classe Disciplina

--- src/main/java/br/upe/sistemas/sisrep/sisrep/core/disciplina/DisciplinaServico.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/core/disciplina/DisciplinaServico.java
@@ -1,19 +1,22 @@
 package br.upe.sistemas.sisrep.sisrep.core.disciplina;
 
 import java.util.List;
+import br.upe.sistemas.sisrep.sisrep.excecao.NaoEncontradoException;
+import br.upe.sistemas.sisrep.sisrep.excecao.SisrepException;
 
 public class DisciplinaServico implements IDisciplinaServico {
 
+  private IDisciplinaRepositorio disciplinaRepositorio;
+
   @Override
   public List<Disciplina> listar() {
-    // TODO Auto-generated method stub
-    return null;
+    return (List<Disciplina>) disciplinaRepositorio.findAll();
   }
 
   @Override
   public Disciplina incluir(Disciplina disciplina) {
-    // TODO Auto-generated method stub
-    return null;
+    validarInclusaoDisciplina(disciplina);
+    return disciplinaRepositorio.save(disciplina);
   }
 
   @Override
@@ -24,8 +27,30 @@ public class DisciplinaServico implements IDisciplinaServico {
 
   @Override
   public void excluir(long id) {
-    // TODO Auto-generated method stub
+    validarExclusaoDisciplina(id);
+    disciplinaRepositorio.deleteById(id);
+  }
 
+  private void validarInclusaoDisciplina(Disciplina disciplina) {
+    if (disciplina == null) {
+      throw new SisrepException("Dados nulos");
+    }
+
+    if (disciplinaRepositorio.findById(disciplina.getId()).isPresent()) {
+      throw new SisrepException(
+          "Ocorreu um erro ao incluir disciplina: já existe uma disciplina cadastrada com esse identificador");
+    }
+  }
+
+  private void validarExclusaoDisciplina(long id) {
+    if (id == 0L) {
+      throw new SisrepException(
+          "Ocorreu um erro ao excluir curso: Informe o identificador correto");
+    }
+
+    if (!disciplinaRepositorio.existsById(id)) {
+      throw new NaoEncontradoException("Ocorreu um erro ao excluir curso: curso não encontrado");
+    }
   }
 
 }
