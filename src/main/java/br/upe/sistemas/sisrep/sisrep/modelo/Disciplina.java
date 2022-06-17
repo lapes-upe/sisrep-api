d0e5d3e1a30b34e1e7ba0cede2bfef7f8a65a2e5
kvojps <joseferreira.santosjunior@upe.br>
2022-06-16 21:57:52 -0300

    adicionando anotacoes a classe Disciplina

--- src/main/java/br/upe/sistemas/sisrep/sisrep/modelo/Disciplina.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/modelo/Disciplina.java
@@ -1,11 +1,31 @@
 package br.upe.sistemas.sisrep.sisrep.modelo;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "disciplina", schema = "public")
 public class Disciplina {
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
   String nome;
   int cargaHoraria;
   double media;
+
   @ManyToOne
   Curso curso;
 }
