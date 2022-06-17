a8f6de067e5eb498dc61621b991a07364a39a460
kvojps <joseferreira.santosjunior@upe.br>
2022-06-16 21:59:21 -0300

    adicionando anotacoes a classe FichaDispensa

--- src/main/java/br/upe/sistemas/sisrep/sisrep/modelo/FichaDispensa.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/modelo/FichaDispensa.java
@@ -1,11 +1,30 @@
 package br.upe.sistemas.sisrep.sisrep.modelo;
 
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToMany;
-import br.upe.sistemas.sisrep.controleacesso.core.usuario.Usuario;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ficha_dispensa", schema = "public")
 public class FichaDispensa {
-  Usuario usuario;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+
+  // Usuario usuario;
+
   @ManyToMany
   List<Disciplina> disciplinas;
   StatusReaproveitamentoDisciplinaEnum status;
