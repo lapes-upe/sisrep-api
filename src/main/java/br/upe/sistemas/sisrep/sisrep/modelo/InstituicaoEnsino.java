50c21e6c75dcdc4b8a97585692c1f6f3a47837fe
kvojps <joseferreira.santosjunior@upe.br>
2022-06-16 21:53:51 -0300

    adicionando anotacoes a classe InstituicaEnsino

--- src/main/java/br/upe/sistemas/sisrep/sisrep/modelo/InstituicaoEnsino.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/modelo/InstituicaoEnsino.java
@@ -1,10 +1,29 @@
 package br.upe.sistemas.sisrep.sisrep.modelo;
 
 import java.util.List;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ies", schema = "public")
 public class InstituicaoEnsino {
-  String nome;
+  @Id
+  @GeneratedValue(strategy = GenerationType.AUTO)
+  private Long id;
+  private String nome;
+
   @OneToMany(mappedBy = "instituicao_nsino")
-  List<Curso> cursos;
+  private List<Curso> cursos;
 }
