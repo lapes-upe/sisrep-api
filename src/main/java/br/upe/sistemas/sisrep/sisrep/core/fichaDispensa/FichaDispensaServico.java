33dc6fb9408a7ceb168bf7f4ae9683d4ca4282ef
kvojps <joseferreira.santosjunior@upe.br>
2022-06-17 19:13:52 -0300

    adicionando servicos a classe FichaDisciplina

--- src/main/java/br/upe/sistemas/sisrep/sisrep/core/fichaDispensa/FichaDispensaServico.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/core/fichaDispensa/FichaDispensaServico.java
@@ -1,19 +1,24 @@
 package br.upe.sistemas.sisrep.sisrep.core.fichaDispensa;
 
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import br.upe.sistemas.sisrep.sisrep.excecao.NaoEncontradoException;
+import br.upe.sistemas.sisrep.sisrep.excecao.SisrepException;
 
 public class FichaDispensaServico implements IFichaDispensaServico {
 
+  @Autowired
+  IFichaDispensaRepositorio fichaRepositorio;
+
   @Override
   public List<FichaDispensa> listar() {
-    // TODO Auto-generated method stub
-    return null;
+    return (List<FichaDispensa>) fichaRepositorio.findAll();
   }
 
   @Override
   public FichaDispensa incluir(FichaDispensa fichaDispensa) {
-    // TODO Auto-generated method stub
-    return null;
+    validarInclusaoFichaDispensa(fichaDispensa);
+    return fichaRepositorio.save(fichaDispensa);
   }
 
   @Override
@@ -24,8 +29,31 @@ public class FichaDispensaServico implements IFichaDispensaServico {
 
   @Override
   public void excluir(long id) {
-    // TODO Auto-generated method stub
+    validarExclusaoFichaDispensa(id);
+    fichaRepositorio.deleteById(id);
+  }
 
+  private void validarInclusaoFichaDispensa(FichaDispensa ficha) {
+    if (ficha == null) {
+      throw new SisrepException("Dados nulos");
+    }
+
+    if (fichaRepositorio.findByUsuario(ficha.getUsuario()).isPresent()) {
+      throw new SisrepException(
+          "Ocorreu um erro ao incluir ficha de dispensa: já existe uma ficha de dispensa cadastrada para esse usuário.");
+    }
+  }
+
+  private void validarExclusaoFichaDispensa(Long id) {
+    if (id == null) {
+      throw new SisrepException(
+          "Ocorreu um erro ao excluir ficha de dispensa: Informe o identificador correto");
+    }
+
+    if (!fichaRepositorio.existsById(id)) {
+      throw new NaoEncontradoException(
+          "Ocorreu um erro ao excluir ficha de dispensa: ficha de dispensa não encontrada");
+    }
   }
 
 }
--- src/main/java/br/upe/sistemas/sisrep/sisrep/core/fichaDispensa/IFichaDispensaRepositorio.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/core/fichaDispensa/IFichaDispensaRepositorio.java
@@ -1,9 +1,9 @@
 package br.upe.sistemas.sisrep.sisrep.core.fichaDispensa;
 
-import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import br.upe.sistemas.sisrep.controleacesso.core.usuario.Usuario;
 
 public interface IFichaDispensaRepositorio extends JpaRepository<FichaDispensa, Long> {
-  List<FichaDispensa> findByUsuario(Usuario usuario);
+  Optional<FichaDispensa> findByUsuario(Usuario usuario);
 }
