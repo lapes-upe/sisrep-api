851b5370fa3b5921091983e288f6276e3e945126
kvojps <joseferreira.santosjunior@upe.br>
2022-06-22 19:00:06 -0300

    adicionando entidade FichaDispensa

--- src/main/java/br/upe/sistemas/sisrep/sisrep/core/reaproveitamentoDisciplina/fichaDispensa/FichaDispensa.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/core/reaproveitamentoDisciplina/fichaDispensa/FichaDispensa.java
@@ -1,5 +1,27 @@
 package br.upe.sistemas.sisrep.sisrep.core.reaproveitamentoDisciplina.fichaDispensa;
 
-public class FichaDispensa {
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+import br.upe.sistemas.sisrep.controleacesso.core.usuario.Usuario;
+import br.upe.sistemas.sisrep.sisrep.core.reaproveitamentoDisciplina.dispensa.Dispensa;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "ficha_dispensa", schema = "public")
+public class FichaDispensa {
+  private Usuario requerente;
+  private String emailReceptorSolicitacao;
+  private String emailEmissorParecer;
+  private String emailAnalistaParecer;
+
+  @OneToOne
+  private Dispensa dispensaDisciplina;
 }
