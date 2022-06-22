04205f05a43abdf235a65b0301160d55d7b028b2
kvojps <joseferreira.santosjunior@upe.br>
2022-06-22 19:06:11 -0300

    adicionando validacoes aos atributos da malha curricular

--- src/main/java/br/upe/sistemas/sisrep/sisrep/core/malhaCurricular/MalhaCurricular.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/core/malhaCurricular/MalhaCurricular.java
@@ -9,6 +9,7 @@ import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
+import javax.validation.constraints.NotBlank;
 import org.joda.time.DateTime;
 import br.upe.sistemas.sisrep.sisrep.core.curso.Curso;
 import br.upe.sistemas.sisrep.sisrep.core.disciplina.Disciplina;
@@ -28,12 +29,16 @@ public class MalhaCurricular {
   @GeneratedValue(strategy = GenerationType.AUTO)
   private Long id;
 
+  @NotBlank(message = "O código é obrigatório")
   private String codigo;
 
+  @NotBlank(message = "O ano de implantação é obrigatório")
   private DateTime anoImplantacao;
 
+  @NotBlank(message = "O parecer CEE é obrigatório")
   private File parecerCEE;
 
+  @NotBlank(message = "O curso é obrigatório")
   @ManyToOne
   private Curso curso;
 
