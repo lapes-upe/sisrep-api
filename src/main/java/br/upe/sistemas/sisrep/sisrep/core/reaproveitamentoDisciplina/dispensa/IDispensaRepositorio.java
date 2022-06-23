88efaf7e7a4b9952d501d1e4b8923d76de405af1
kvojps <joseferreira.santosjunior@upe.br>
2022-06-22 23:43:24 -0300

    refatorando DispensaRepositorio

--- src/main/java/br/upe/sistemas/sisrep/sisrep/core/reaproveitamentoDisciplina/dispensa/IDispensaRepositorio.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/core/reaproveitamentoDisciplina/dispensa/IDispensaRepositorio.java
@@ -1,9 +1,10 @@
 package br.upe.sistemas.sisrep.sisrep.core.reaproveitamentoDisciplina.dispensa;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IDispensaRepositorio extends JpaRepository<Dispensa, Long> {
-  // Optional<Dispensa> findByUsuario(Usuario usuario);
+  Optional<Dispensa> findById(Long id);
 }
