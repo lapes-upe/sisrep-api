e9283fee7f1ed476efb5dba65159b3d42ea7d341
kvojps <joseferreira.santosjunior@upe.br>
2022-06-23 00:16:15 -0300

    adicionando malhaCurricularServico

--- src/main/java/br/upe/sistemas/sisrep/sisrep/core/malhaCurricular/IMalhaCurricularRepositorio.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/core/malhaCurricular/IMalhaCurricularRepositorio.java
@@ -1,10 +1,11 @@
 package br.upe.sistemas.sisrep.sisrep.core.malhaCurricular;
 
 import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface IMalhaCurricularRepositorio extends JpaRepository<MalhaCurricular, Long> {
-  List<MalhaCurricular> findByCodigo(String codigo);
+  Optional<List<MalhaCurricular>> findByCodigo(String codigo);
 }
--- src/main/java/br/upe/sistemas/sisrep/sisrep/core/malhaCurricular/MalhaCurricularServico.java
+++ src/main/java/br/upe/sistemas/sisrep/sisrep/core/malhaCurricular/MalhaCurricularServico.java
@@ -1,8 +1,62 @@
 package br.upe.sistemas.sisrep.sisrep.core.malhaCurricular;
 
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import br.upe.sistemas.sisrep.sisrep.excecao.NaoEncontradoException;
+import br.upe.sistemas.sisrep.sisrep.excecao.SisrepException;
 
 @Service
-public class MalhaCurricularServico {
+public class MalhaCurricularServico implements IMalhaCurricularServico {
+
+  @Autowired
+  private IMalhaCurricularRepositorio malhaRepositorio;
+
+  @Override
+  public List<MalhaCurricular> listar() {
+    return (List<MalhaCurricular>) malhaRepositorio.findAll();
+  }
+
+  @Override
+  public MalhaCurricular incluir(MalhaCurricular malha) {
+    validarInclusaoMalha(malha);
+    return malhaRepositorio.save(malha);
+  }
+
+  @Override
+  public MalhaCurricular alterar(MalhaCurricular malha) {
+    // TODO Auto-generated method stub
+    return null;
+  }
+
+  @Override
+  public void excluir(Long id) {
+    validarExclusaoMalha(id);
+    malhaRepositorio.deleteById(id);
+  }
+
+  private void validarInclusaoMalha(MalhaCurricular malha) {
+    if (malha == null) {
+      throw new SisrepException("Dados nulos");
+    }
+
+    if (malhaRepositorio.findByCodigo(malha.getCodigo()).isPresent()) {
+      throw new SisrepException(
+          "Ocorreu um erro ao incluir o usuário: já existe uma instituição de ensino cadastrada com esse nome"
+              + malha.getCodigo());
+    }
+  }
+
+  private void validarExclusaoMalha(Long id) {
+    if (id == null) {
+      throw new SisrepException(
+          "Ocorreu um erro ao excluir instituição de ensino: Informe o identificador correto");
+    }
+
+    if (!malhaRepositorio.existsById(id)) {
+      throw new NaoEncontradoException(
+          "Ocorreu um erro ao excluir instituição de ensino: instituição de ensino não encontrada");
+    }
+  }
 
 }
